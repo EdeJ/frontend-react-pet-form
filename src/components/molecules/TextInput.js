322e0e12bd8c9983c29130398d3a042a29e8a25d
Emiel de Jong <emieldejong@xs4all.nl>
2020-11-26 12:39:49 +0100

    update

--- src/components/molecules/TextInput.js
+++ src/components/molecules/TextInput.js
@@ -3,11 +3,11 @@ import { ErrorMessage } from '../atoms/ErrorMessage'
 import { Input } from '../atoms/Input'
 import { Label } from '../atoms/Label'
 
-export const TextInput = ({ name, label, fieldRef, errors }) => {
+export const TextInput = ({ name, label, type, fieldRef, errors }) => {
     return (
         <div>
             <Label name={name}>{label}</Label>
-            <Input type="text" name={name} fieldRef={fieldRef} />
+            <Input type={type} name={name} fieldRef={fieldRef} />
             {errors[name] && <ErrorMessage>{errors[name].message || "Error"}</ErrorMessage>}
         </div>
     )
--- src/components/organisms/Form.js
+++ src/components/organisms/Form.js
@@ -15,16 +15,90 @@ export const Form = () => {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <TextInput
+                type="text"
                 label="Voornaam"
                 name="name"
                 fieldRef={register({
                     required: {
                         value: true,
-                        message: "Verplicht veld"
+                        message: "Er is geen voornaam ingevuld."
                     }
                 })}
                 errors={errors}
             />
+            <TextInput
+                type="text"
+                label="Achternaam"
+                name="lastName"
+                fieldRef={register({
+                    required: {
+                        value: true,
+                        message: "Er is geen achternaam ingevuld."
+                    }
+                })}
+                errors={errors}
+            />
+            <TextInput
+                type="number"
+                label="Leeftijd"
+                name="age"
+                fieldRef={register({
+                    required: {
+                        value: true,
+                        message: "Er is geen leeftijd ingevuld."
+                    },
+                    min: {
+                        value: 18,
+                        message: "Je moet minimaal 18 jaar zijn."
+                    }
+                })}
+                errors={errors}
+            />
+            <TextInput
+                type="string"
+                label="Postcode"
+                name="zipCode"
+                fieldRef={register({
+                    required: {
+                        value: true,
+                        message: "Er is geen postcode ingevuld."
+                    },
+                    pattern: {
+                        value: /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i,
+                        message: "Postcode is onjuist."
+                    }
+                })}
+                errors={errors}
+            />
+            <TextInput
+                type="string"
+                label="Huisnummer"
+                name="houseNumber"
+                fieldRef={register({
+                    required: {
+                        value: true,
+                        message: "Er is geen huisnummer ingevuld."
+                    },
+                    maxLength: {
+                        value: 8,
+                        message: "Huisnummer is te lang"
+                    }
+                })}
+                errors={errors}
+            />
+
+            <p>Selecteer je favoriete huisdier:</p>
+            <fieldset name="pet" ref={register({ required: true })}>
+                <label htmlFor="pet">Kat</label>
+                <input type="radio" id="cat" name="pet" value="cat" ref={register({ required: true })} />
+                <label htmlFor="pet">Hond</label>
+                <input type="radio" id="dog" name="pet" value="dog" ref={register({ required: true })} />
+                <label htmlFor="hamster">Hamster</label>
+                <input type="radio" id="hamster" name="pet" value="hamster" ref={register({ required: true })} />
+                <label htmlFor="other">Anders</label>
+                <input type="radio" id="other" name="pet" value="other" ref={register({ required: true })} />
+            </fieldset>
+
             <input type="submit" />
         </form>
     )
