d1c71e5e8267a3c8aac4659b005d530aa0418a43
Emiel de Jong <emieldejong@xs4all.nl>
2020-11-27 18:18:13 +0100

    radio buttons inline

--- src/components/molecules/radioGroup/index.jsx
+++ src/components/molecules/radioGroup/index.jsx
@@ -3,9 +3,11 @@ import { ErrorMessage, Label } from '../../atoms'
 
 export const RadioGroup = ({ children, label, name, errors }) => {
     return (
-        <div>
+        <div className="input-section">
             <Label>{label}</Label>
-            {children}
+            <div className="radio-group">
+                {children}
+            </div>
             {errors[name] && <ErrorMessage>{errors[name].message || "Error"}</ErrorMessage>}
         </div>
     )
--- src/components/molecules/radioInput/index.jsx
+++ src/components/molecules/radioInput/index.jsx
@@ -4,8 +4,8 @@ import { Input, Label } from '../../atoms'
 export const RadioInput = ({ label, name, value, fieldRef }) => {
     return (
         <div>
-            <Label name={name}>{label}</Label>
             <Input type="radio" name={name} fieldRef={fieldRef} value={value} />
+            <Label name={name}>{label}</Label>
         </div>
     )
 }
